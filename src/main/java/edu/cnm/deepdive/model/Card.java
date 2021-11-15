c37b242c50129f79b1dbe05fb9d9cc9eceb5a1ff
Todd Nordquist <toddnordquist144@gmail.com>
2021-11-14 19:43:59 -0700

    Implement natural ordering (Comparable) for Card.

--- src/main/java/edu/cnm/deepdive/model/Card.java
+++ src/main/java/edu/cnm/deepdive/model/Card.java
@@ -1,6 +1,6 @@
 package edu.cnm.deepdive.model;
 
-public class Card {
+public class Card implements Comparable<Card>{
 
   private final Rank rank;
   private final Suit suit;
@@ -23,4 +23,12 @@ public class Card {
     return String.format("%s %s", rank.getSymbol(), suit.getSymbol());
   }
 
+  @Override
+  public int compareTo(Card other) {
+    int comparison = suit.compareTo(other.suit);
+    if(comparison == 0) {
+      comparison = rank.compareTo(other.rank);
+    }
+    return comparison;
+  }
 }
